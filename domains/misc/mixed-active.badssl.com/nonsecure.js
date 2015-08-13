b583b00f3f52bff5462bd2d13fad210734108cad
Richard Barnes <rlb@ipv.sx>
2015-08-13 08:53:23 -0700

    Files for new page

---
+++ domains/misc/mixed-active.badssl.com/nonsecure.js
@@ -0,0 +1,8 @@
+(function() {
+  // Change the background to red
+  document.querySelector("html").style.backgroundColor = "#f00";
+  document.querySelector("body").style.backgroundColor = "#f00";
+
+  // Write text to the page
+  document.getElementById("mixed").innerHTML = "[Active mixed content loaded]";
+})();
