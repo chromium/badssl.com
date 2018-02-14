c2556ae691d99b042b2c7667f1c521a2b69777c9
April King <april@pokeinthe.io>
2018-02-14 12:33:46 -0600

    Add support for an extended validation certificate

--- domains/misc/badssl.com/dashboard/sets.js
+++ domains/misc/badssl.com/dashboard/sets.js
@@ -56,6 +56,7 @@ var sets = [
       {subdomain: "rsa2048"},
       {subdomain: "ecc256"},
       {subdomain: "ecc384"},
+      {subdomain: "extended-validation"},
       {subdomain: "mozilla-modern"}
     ]
   },
