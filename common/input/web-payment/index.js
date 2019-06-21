bc5381d172bc7f76179d6357eaf6bf05597c2761
Rouslan Solomakhin <rouslan@google.com>
2019-06-21 16:39:58 -0400

    [web-payment] Print API return values and exceptions in the footer. (#392)
    * Print API return values and exceptions in the footer.
    
    For easier debugging on mobile devices, print the API return values and
    exception messages in a default-hidden section and add a toggle button.

--- common/input/web-payment/index.js
+++ common/input/web-payment/index.js
@@ -1,3 +1,31 @@
+function appendLog(msg) {
+  console.log(msg);
+  let logList = document.getElementById('log');
+  let logEntry = document.createElement('li');
+  let logText = document.createTextNode(msg);
+  logEntry.appendChild(logText);
+  logList.appendChild(logEntry);
+}
+
+function clearLog() {
+  let logList = document.getElementById('log');
+  while (logList.firstChild) {
+    logList.removeChild(logList.firstChild);
+  }
+}
+
+function toggleLogVisibility() {
+  let logList = document.getElementById('log');
+  let logToggle = document.getElementById('log-toggle');
+  if (logList.hidden) {
+    logToggle.innerHTML = 'Hide log';
+    logList.hidden = false;
+  } else {
+    logToggle.innerHTML = 'Show log';
+    logList.hidden = true;
+  }
+}
+
 /**
  * Builds PaymentRequest for credit cards, but does not show any UI yet.
  * @return {PaymentRequest} The PaymentRequest object.
@@ -17,9 +45,14 @@ function initPaymentRequest() {
         },
       });
   request.canMakePayment().then(function(result) {
-    console.log(result);
+    appendLog('canMakePayment returned: ' + result);
   }).catch(function(err) {
-    console.log(err);
+    appendLog('canMakePayment rejected: ' + err.name + ': ' + err.message);
+  });
+  request.hasEnrolledInstrument().then(function(result) {
+    appendLog('hasEnrolledInstrument returned: ' + result);
+  }).catch(function(err) {
+    appendLog('hasEnrolledInstrument rejected: ' + err.name + ': ' + err.message);
   });
   return request;
 }
@@ -28,13 +61,15 @@ let request = initPaymentRequest();
 
 /** Invokes PaymentRequest for credit cards. */
 function handleClick() {
-  request.show()
-      .then(function(instrumentResponse) {
-        return instrumentResponse.complete('success');
-      })
-      .catch(function(err) {
-        console.log(err);
-      });
+  clearLog();
+  request.show().then(function(instrumentResponse) {
+    appendLog('show returned: ' + JSON.stringify(instrumentResponse));
+    request = initPaymentRequest();
+    return instrumentResponse.complete('success');
+  })
+  .catch(function(err) {
+    appendLog('show rejected: ' + err.name + ': ' + err.message);
+    request = initPaymentRequest();
+  });
 
-  request = initPaymentRequest();
 }
